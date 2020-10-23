7341479931ed05122261d309eb2576331197c9cf
hpkn <paulhassang@hotmail.com>
2020-10-23 14:10:52 +0900

    paul

--- api.js
+++ api.js
@@ -1,68 +1,31 @@
+//엄격한 코드 검사
+'use strict';
 
+/************* include library **************/
+var express = require('express');
+var api     = express();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-var dbInfo = {
-
-    host: 'ls-4ab94082220316df9f6805e2cb989baaea4e9715.cu0xyssgzj43.ap-northeast-2.rds.amazonaws.com',
-    port: '3306',
-    user: 'dbmasteruser',
-    password:'angksehwjsqq',
-    database: 'BU',
-    multipleStatements: true
-});
-Connection.connect();
-Connection.query('SELECT * FROM sensor_data;, function(error,results,fields) {
-    (error) {
-        console,log(error);
-    }
-    console,log(results);
-    });
-    Connection.end();
-    res.send("welcome is API Function")
+/************* Routing **************/
+//api Index
+api.get('/', (req, res, next) => {
+    res.send("Welcome is API Fucntion");
 });
 
-api.post('/insSensor', (req, res, next) => {
-
-    var sensorType = req.body.sensorType;// "";
-    var sensorValue = req.body.sensorValue;//"";
-    var userId = req.body.userId; //"";
-    
-    var sql = " insert into sensor_data (sensor_type, sensor_value, sensor_usr_id, ins_date, upd_date ) values ";
-    sql += " ('"+ sensorType +"', "+ sensorValue +", '"+ userId +"', now() , now()) ";
-    console.log(sql);
-    connection.connect();
-
-    console.log("init start");
-    connection.query(sql , func
-    connection.query(sql , function(error, results, fields){
-
-        console.log(error);
-        console.log(results);
-        res.send(results);
-    })
-
-    //req.body.sensorIdx
-    //req.body.sensorType
-    //req.body.sensorValue
-
+/************* Routing **************/
+//api Index
+api.get('/hello', (req, res, next) => {
+    res.send("HTTP GET > Hello Nodejs");
 });
+
+api.post('/hello', (req, res, next) => {
+    res.send("HTTP POST > Hello Nodejs");
+});
+
+//Query String
+// ex) http://localhost/api/echo?param1=123&param2=321
+api.get('/query_echo', (req, res, next) => {
+    res.send(req.query);
+});
+
+
+module.exports = api;
