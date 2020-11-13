607587be233e5ae5bf45a36619b900bde6b95309
hpkn <paulhassang@hotmail.com>
2020-11-13 12:42:28 +0900

    commit

--- api1.js
+++ api1.js
@@ -7,38 +7,46 @@ var mysql = require('mysql')
 
 var dbInfo = {
     
-    host: 'ls-712a3de0f216372c332622b5ed5c6f22fe2f67bd.cu0xyssgzj43.ap-northeast-2.rds.amazonaws.com',
-    port: '3306',
-    user: 'dbmasteruser',
-    password: 'buackr!!##',
-    database: 'BU',
-    multipleStatements: true
-}
-var  Connection = mysql.createConnection({
-    host : dbInfo.host,
-    user : dbInfo.user,
-    password : dbInfo.password,
-    database : dbInfo.database
-})
-Connection.connect();
-Connection.query('SELECT * FROM sensor_data', function(error,results,fields){
-    if (error) {
-        console.log(error);
+        host: 'ls-712a3de0f216372c332622b5ed5c6f22fe2f67bd.cu0xyssgzj43.ap-northeast-2.rds.amazonaws.com',
+        port: '3306',
+        user: 'dbmasteruser',
+        password: 'buackr!!##',
+        database: 'BU',
+        multipleStatements: true
     }
-
-    console.log(results);
     
+    var Connection = mysql.createConnection({
+        host : dbInfo.host,
+        user : dbInfo.user,
+        password: dbInfo.password,
+        database : dbInfo.database
+    });
+
+ 
+    Connection.connect();
+    Connection.query('SELECT * FROM sensor_data', function(error,results,fields) {
+
+    if (error) {
+    console.log(error);
+    }
+    console.log(results);
     Connection.end();
    // res.send("welcome is API Function")
 });
+    
 
-api.post('/insSensor', (req, res, next) => {
+api.get('/insSensor', (req, res, next) => {
 
     var sensorType = req.body.sensorType;// "";
     var sensorValue = req.body.sensorValue;//"";
     var userId = req.body.userId; //"";
     
     var sql = " insert into sensor_data (sensor_type, sensor_value, sensor_usr_id, ins_date, upd_date ) values ";
+
+    //ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'
+   
+
+
     sql += " ('"+ sensorType +"', "+ sensorValue +", '"+ userId +"', now() , now()) ";
     console.log(sql);
     connection.connect();
@@ -50,11 +58,12 @@ api.post('/insSensor', (req, res, next) => {
         console.log(results);
         res.send(results);
     })
-
-    //req.body.sensorIdx
-    //req.body.sensorType
-    //req.body.sensorValue
+    
+    req.body.sensorIdx
+    req.body.sensorType
+    req.body.sensorValue
 
 });
+
 
 module.exports = api;
